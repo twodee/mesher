7150daa66ed3b315950005255c15345dcd19dd05
Chris Johnson <crjjrc@gmail.com>
2019-10-17 08:15:42 -0500

    Add trackball.

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ let attriutes;
 let shaderProgram;
 let projection;
 let modelview;
+let trackball;
 
 function initialize() {
   canvas = document.getElementById('canvas');
@@ -42,11 +43,22 @@ void main() {
   shaderProgram = new ShaderProgram(vertexSource, fragmentSource);
   vertexArray = new VertexArray(shaderProgram, attributes);
 
+  trackball = new Trackball();
   // modelview = Matrix4.translate(-0.1, 0.1, 0);
   modelview = Matrix4.rotate(Vector4.forward(), 45);
 
   resizeWindow();
+
+  // Register callbacks.
+  canvas.addEventListener('mousedown', mouseDown);
+  canvas.addEventListener('mouseup', mouseUp);
+
+  // This goes on the window rather than the canvas so that drags can keep
+  // going even when the mouse goes off the canvas.
+  window.addEventListener('mousemove', mouseMove);
 }
+
+// --------------------------------------------------------------------------- 
 
 function render() {
   gl.clearColor(0, 0, 0, 1);
@@ -55,7 +67,8 @@ function render() {
 
   shaderProgram.bind();
   shaderProgram.setUniformMatrix4('projection', projection);
-  shaderProgram.setUniformMatrix4('modelview', modelview);
+  shaderProgram.setUniformMatrix4('modelview', trackball.rotation);
+  // shaderProgram.setUniformMatrix4('modelview', new Matrix4());
 
   vertexArray.bind();
   vertexArray.drawSequence(gl.TRIANGLES);
@@ -67,14 +80,31 @@ function render() {
 function resizeWindow() {
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
+  trackball.setViewport(canvas.width, canvas.height);
   updateProjection();
   render();
 }
 
 function updateProjection() {
   let windowAspect = canvas.width / canvas.height;
-  projection = Matrix4.ortho(0, 1, 0, 1);
+  projection = Matrix4.ortho(-1, 1, -1, 1);
 }
+
+function mouseDown(e) {
+  trackball.start(e.clientX, e.clientY);
+}
+
+function mouseMove(e) {
+  if (e.buttons === 1) {
+    trackball.drag(e.clientX, e.clientY);
+    render();
+  }
+}
+
+function mouseUp(e) {
+}
+
+// --------------------------------------------------------------------------- 
 
 window.addEventListener('load', initialize);
 window.addEventListener('resize', resizeWindow, false);
