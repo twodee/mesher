74ecf410ad83cfbaa41732e65d7c271bcd5ccbca
Chris Johnson <crjjrc@gmail.com>
2020-05-29 13:39:34 -0500

    Add zooming, trimesh.

--- client.js
+++ client.js
@@ -6,96 +6,24 @@ let shaderProgram;
 let projection;
 let modelview;
 let trackball;
+let trimesh;
+let zoom;
+
+// --------------------------------------------------------------------------- 
 
 function initialize() {
   canvas = document.getElementById('canvas');
   gl = canvas.getContext('webgl2');
+  zoom = -10;
+
+  trimesh = TrimeshIO.readObj('/Users/johnch/Desktop/cylinder.obj');
+  // trimesh = Prefab.cube();
 
   attributes = new VertexAttributes();
+  attributes.addAttribute('vposition', trimesh.vertexCount, 4, trimesh.getFlatPositions());
+  attributes.addAttribute('vnormal', trimesh.vertexCount, 4, trimesh.getFlatNormals());
+  attributes.addIndices(trimesh.getFlatFaces());
 
-  let positions = [
-    0, 0, 1, 1,
-    1, 0, 1, 1,
-    0, 1, 1, 1,
-    1, 1, 1, 1,
-
-    0, 0, 0, 1,
-    1, 0, 0, 1,
-    0, 1, 0, 1,
-    1, 1, 0, 1,
-
-    1, 0, 1, 1,
-    1, 0, 0, 1,
-    1, 1, 1, 1,
-    1, 1, 0, 1,
-
-    0, 0, 0, 1,
-    0, 0, 1, 1,
-    0, 1, 0, 1,
-    0, 1, 1, 1,
-
-    0, 1, 1, 1,
-    1, 1, 1, 1,
-    0, 1, 0, 1,
-    1, 1, 0, 1,
-
-    0, 0, 0, 1,
-    1, 0, 0, 1,
-    0, 0, 1, 1,
-    1, 0, 1, 1,
-  ];
-
-  let normals = [
-    0, 0, 1, 0,
-    0, 0, 1, 0,
-    0, 0, 1, 0,
-    0, 0, 1, 0,
-
-    0, 0, -1, 0,
-    0, 0, -1, 0,
-    0, 0, -1, 0,
-    0, 0, -1, 0,
-
-    1, 0, 0, 0,
-    1, 0, 0, 0,
-    1, 0, 0, 0,
-    1, 0, 0, 0,
-
-    -1, 0, 0, 0,
-    -1, 0, 0, 0,
-    -1, 0, 0, 0,
-    -1, 0, 0, 0,
-
-    0, 1, 0, 0,
-    0, 1, 0, 0,
-    0, 1, 0, 0,
-    0, 1, 0, 0,
-
-    0, -1, 0, 0,
-    0, -1, 0, 0,
-    0, -1, 0, 0,
-    0, -1, 0, 0,
-  ];
-
-  let indices = [
-    0, 1, 3,
-    0, 3, 2,
-    5, 4, 6,
-    5, 6, 7,
-    8, 9, 11,
-    8, 11, 10,
-    12, 13, 15,
-    12, 15, 14,
-    16, 17, 19,
-    16, 19, 18,
-    20, 21, 23,
-    20, 23, 22,
-  ];
-
-  attributes.addAttribute('vposition', 24, 4, positions);
-  attributes.addAttribute('vnormal', 24, 4, normals);
-  attributes.addIndices(indices);
-  
   let vertexSource = `#version 300 es
 uniform mat4 projection;
 uniform mat4 modelview;
@@ -111,7 +39,7 @@ void main() {
   gl_Position = projection * modelview * vposition;
 
   fposition = (modelview * vposition).xyz;
-  fcolor = vposition.xyz;
+  fcolor = vec3(1.0);
   fnormal = normalize((modelview * vnormal).xyz);
 }
   `;
@@ -147,6 +75,7 @@ void main() {
   // Register callbacks.
   canvas.addEventListener('mousedown', mouseDown);
   canvas.addEventListener('mouseup', mouseUp);
+  canvas.addEventListener('wheel', mouseWheel);
 
   // This goes on the window rather than the canvas so that drags can keep
   // going even when the mouse goes off the canvas.
@@ -157,15 +86,18 @@ void main() {
 
 function render() {
   gl.clearColor(0, 0, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  const center = Matrix4.translate(-trimesh.centroid.x, -trimesh.centroid.y, -trimesh.centroid.z);
 
   shaderProgram.bind();
   shaderProgram.setUniformMatrix4('projection', projection);
-  shaderProgram.setUniformMatrix4('modelview', Matrix4.translate(0, 0, -10).multiplyMatrix(trackball.rotation).multiplyMatrix(Matrix4.translate(-0.5, -0.5, -0.5)));
+  shaderProgram.setUniformMatrix4('modelview', Matrix4.translate(0, 0, zoom).multiplyMatrix(trackball.rotation).multiplyMatrix(center));
   // shaderProgram.setUniformMatrix4('modelview', new Matrix4());
 
   vertexArray.bind();
+  // vertexArray.drawSequence(gl.POINTS);
   vertexArray.drawIndexed(gl.TRIANGLES);
   vertexArray.unbind();
 
@@ -198,6 +130,14 @@ function mouseMove(e) {
 }
 
 function mouseUp(e) {
+}
+
+function mouseWheel(e) {
+  zoom += e.wheelDelta * 0.01;
+  if (zoom >= 0) {
+    zoom = -0.01;
+  }
+  render();
 }
 
 // --------------------------------------------------------------------------- 
